d2f59ea244ff7b3e06a887b2e7d0075b2a6bcaba
Eufrat Tsaqib <eutsat@gmail.com>
2019-02-03 20:04:22 +0700

    Move tensorboard command

--- jupyter-keras-gpu/run_jupyter.sh
+++ jupyter-keras-gpu/run_jupyter.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 jupyter lab "$@"
+tensorboard --logdir=/notebooks/logs --host 0.0.0.0
--- jupyter-keras-gpu/run_tensorboard.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-tensorboard --logdir=/notebooks/logs --host 0.0.0.0
